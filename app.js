c2061b015a837e524ede78863cdacf07d5cd0389
Hamza <hamza.labzioui@gmail.com>
2022-06-29 23:10:47 +0200

    2nd commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const favicon = require('serve-favicon')
+const sequelize = require('./src/db/sequelize')
+
+const app = express()
+const port = 3000
+
+app
+    .use(favicon(__dirname + '/favicon.ico'))
+    .use(morgan('dev'))
+    .use(bodyParser.json())
+
+
+sequelize.initDb()
+
+require('./src/routes/findAllPokemons')(app)
+require('./src/routes/findPokemonByPk')(app)
+require('./src/routes/createPokemon')(app)
+require('./src/routes/updatePokemon')(app)
+require('./src/routes/deletePokemon')(app)
+
+
+
+
+app.listen(port, () => console.log(`Notre application Node est démarée sur : http://localhost:${port}`));
